b5a30bff35d8ab0da92d68d93a8e6de254204201
Ricky Codings <cselabmaterials@gmail.com>
2024-01-22 13:24:20 +0530

    Changed the files into new for the program setup

--- src/App.js
+++ src/App.js
@@ -1,15 +1,9 @@
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
 
-        Hello Richardson :_)
-        weclome to Weather app 
-
-      </header>
     </div>
   );
 }
